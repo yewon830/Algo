79bf591dda8e7b7ac241651c8b6a2260800b8245
Ye Won Jeong <wonderwall00@naver.com>
2023-12-19 18:10:59 +0900

    [Silver IV] Title: ATM, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -1,10 +1,7 @@
-# 최소->최대 정렬
-
 N = int(input())
-P = sorted(list(map(int, input().split())))
+numList = list(map(int,input().split()))
 
-# 누적합.
+numList.sort()
 for i in range(1,N):
-    P[i] += P[i-1]
-
-print(sum(P))
+    numList[i] += numList[i-1]
+print(sum(numList))
