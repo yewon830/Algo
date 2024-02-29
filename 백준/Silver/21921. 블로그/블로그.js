a4094b7541cd9ad2d87061793ad76a5064e6a962
Ye Won Jeong <wonderwall00@naver.com>
2024-02-29 16:34:46 +0900

    [Silver III] Title: 블로그, Time: 288 ms, Memory: 37080 KB -BaekjoonHub

--- 백준/Silver/21921. 블로그/블로그.js
+++ 백준/Silver/21921. 블로그/블로그.js
@@ -1,28 +1,60 @@
-const [[n, x], arr] = require("fs")
+// 구간 합 : 연속된 구간의 합 중 가장 큰 것. + 그 구간의 갯수
+
+const filePath = process.platform === 'linux' ? 
+'/dev/stdin' : './input.txt';
+
+const [[totalDays, days], visitor] = require("fs")
   .readFileSync("/dev/stdin")
   .toString()
   .trim()
   .split("\n")
   .map((line) => line.split(" ").map(Number));
 
-let sum = arr.slice(0, x).reduce((sum, cur) => sum + cur, 0);
-let max = sum;
-let count = 1;
 
-for (let i = 0; i < n - x; i++) {
-  sum -= arr[i];
-  sum += arr[i + x];
-  if (max < sum) {
-    max = sum;
-    count = 1;
-  } else if (max === sum) {
-    count++;
-  }
+let sumVisitor = visitor.slice(0,days).reduce((sum,cur)=>sum+cur,0);
+let maxVisitor = sumVisitor;
+let maxCnt = 1; 
+for(let i=0; i<totalDays-days;i++){
+    sumVisitor -= visitor[i]
+    sumVisitor += visitor[i+days]
+    if(sumVisitor >maxVisitor ){
+        maxVisitor = sumVisitor
+        // 최댓값 갱신이라 갯수도 갱신
+        maxCnt = 1
+    }else if (sumVisitor === maxVisitor){
+        maxCnt++;
+    }
+}
+if(maxVisitor===0){
+    console.log('SAD')
+}else{
+    console.log(maxVisitor);
+    console.log(maxCnt);
 }
 
-if (max === 0) {
-  console.log("SAD");
-} else {
-  console.log(max);
-  console.log(count);
-}
+
+// 시간 초과
+// for (let i=0; i <= totalDays-days; i++){
+//     let countVisitor = 0;
+//     for(let j=0; j < days; j++){
+//         countVisitor += visitor[i+j]
+//     }
+//     if(sumVisitor<countVisitor){
+//         sumVisitor = countVisitor
+//     }
+// }
+// //최댓값 계산 완료
+// for (let i=0; i <= totalDays-days; i++){
+//     let countVisitor = 0;
+//     for(let j=0; j < days; j++){
+//         countVisitor += visitor[i+j]
+//     }
+//     if(countVisitor === sumVisitor){
+//         maxCnt += 1
+//     }
+
+// }
+
+
+
+
