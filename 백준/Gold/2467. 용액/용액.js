6a47ded02ed81c74c581ae770a671e5c8e71ac54
Ye Won Jeong <wonderwall00@naver.com>
2024-03-13 23:26:06 +0900

    [Gold V] Title: 용액, Time: 232 ms, Memory: 21536 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.js
+++ 백준/Gold/2467. 용액/용액.js
@@ -1,42 +1,33 @@
-// 가장 0에 가까운 용액
-// 오름차순으로 정렬 후 
-// 가장 0에 가까운지 비교
-// 이분 탐색 : 
-// 배열을 돌면서 하나하나씩 중간 값을 찝는다. 
-// 만약 어느 한쪽으로 움직였을 때 절댓값이 더 커지면 그 이후로는 보지 않는다.
-
 const filePath = process.platform === 'linux' ? 
 '/dev/stdin' : './input.txt';
 
 let input = require('fs').readFileSync(filePath).toString().trim().split('\n');
 
-let N = Number(input[0]);
-let liquidsArr = input[1].split(" ").map(Number)
+let N = Number(input[0])
+let liquidsArr = input[1].split(" ").map(Number);
 
-let leftAns = 0;
-let rightAns = 0;
-let absValue = 2000000000;
+let abs = 2000000000;
+let ansLeft = 0;
+let ansRight = 0; 
 
-for(let i = 0; i< N-1;i++){
-    let start = i+1
-    let end = N-1
+for(let i=0; i<N-1;i++){
+    let start = i+1;
+    let end = N-1;
     while(start<=end){
-        let mid = Math.floor((start+end)/2)
-        let sum = liquidsArr[mid]+liquidsArr[i]
-        if(Math.abs(sum) < absValue){
-            leftAns = i
-            rightAns = mid
-            absValue = Math.abs(sum)
+        let mid = Math.floor((start+end)/2);
+        let sum = liquidsArr[mid] +liquidsArr[i]
+        if(Math.abs(sum) < abs){
+            abs = Math.abs(sum);
+            ansLeft = liquidsArr[i]
+            ansRight = liquidsArr[mid]
         }
-        if(sum === 0){
+        if(sum===0){
             break
-        }else if(sum<0){
-            // 음수면, 더 큰쪽으로
-            start = mid +1
+        }else if(sum>0){
+            end = mid - 1;
         }else{
-            end = mid -1
+            start = mid + 1
         }
     }
 }
-
-console.log(`${liquidsArr[leftAns]} ${liquidsArr[rightAns]}`)
+console.log(ansLeft, ansRight)
