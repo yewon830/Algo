cd88f557e0463bf0d546ef24441886a0773b16cb
Ye Won Jeong <wonderwall00@naver.com>
2024-03-19 18:11:25 +0900

    [Gold V] Title: 강의실 배정, Time: 596 ms, Memory: 104200 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.js
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.js
@@ -3,30 +3,32 @@ const filePath = process.platform === 'linux' ?
 
 let input = require('fs').readFileSync(filePath).toString().trim().split('\n');
 
-let lectureCnt = Number(input[0])
-let timesArr = []
-for(let i = 1; i<=lectureCnt;i++){
-    let times = input[i].split(" ").map(Number)
-    timesArr.push([times[0],1]) 
-    timesArr.push([times[1],-1])
-}
-// 수업 끝과 시작이 동시에 발생하면, 수업 종료가 먼저 와야해서 끝시간도 정렬 필요함. 수업이 시작하면 강의 +1 끝나면
 
-// 시작값으로 정렬
-timesArr.sort((a,b)=>{
+let lectureCnt = Number(input[0])
+let lecturesArr = []
+for(let i=1; i<=lectureCnt;i++){
+    let lec = input[i].split(" ").map(Number)
+    lecturesArr.push([lec[0],0])
+    lecturesArr.push([lec[1],1])
+}
+lecturesArr.sort((a,b)=>{
     if(a[0]===b[0]){
-        return a[1]-b[1]
+        return b[1]-a[1]
     }
     return a[0]-b[0]
-
 })
 
-let nowLectureCnt = 0;
-let ans = 0;
-for (let time of timesArr){
-    nowLectureCnt += time[1]
-    if (nowLectureCnt> ans){
-        ans = nowLectureCnt
+let max = -1;
+let rooms = 0;
+for(let i=0; i<lecturesArr.length; i++){
+    if(lecturesArr[i][1]===0){
+        rooms++
+    }else{
+        rooms--
+    }
+    if(rooms > max){
+        max = rooms
     }
 }
-console.log(ans)
+
+console.log(max)
