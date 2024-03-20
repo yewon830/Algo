67d307021a6ca6e0b3c761d594030fcf822198de
Ye Won Jeong <wonderwall00@naver.com>
2024-03-20 23:16:18 +0900

    [Silver I] Title: 회의실 배정, Time: 404 ms, Memory: 43168 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.js
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.js
@@ -4,27 +4,27 @@ const filePath = process.platform === 'linux' ?
 let input = require('fs').readFileSync(filePath).toString().trim().split('\n');
 
 let meetingCnt = Number(input[0]);
-let meetingArr = []
-// 미팅 배열 넣기
-for(let i=1; i<= meetingCnt;i++) meetingArr.push(input[i].trim().split(" ").map(Number))
-
-
-// 미팅을 최대한 많이 하려면, 시작 시간은 중요하지 않고 일단 끝나는 시간이 빨라야 한다.
-// 끝나는 시간 순으로 정렬해서 탐색하고, 다음 첫 시간이 끝시간보다 같거나 나중이라면 바로 채택.
-
-// meetingArr을 내부 배열의 [1]번째 값 기준으로 정렬해야 함. 
-meetingArr = meetingArr.sort((a,b)=>{
+// 배열을 돌면서 뒷쪽을 오름차순으로 정렬
+// 만약 같다면 앞쪽도 오름차순으로 정렬. 
+// 이전의 끝시간과 지금의 앞시간을 비교하면서, 만약 앞시간이 같거나 크다면 +1
+let meetingArr = [];
+for(let i=1;i<=meetingCnt;i++){
+    meetingArr.push(input[i].split(" ").map(Number))
+}
+meetingArr.sort((a,b)=>{
     if(a[1]===b[1]){
-        return a[0] - b[0];
-    }else{
-        return a[1]-b[1];
+        return a[0]-b[0];
     }
-    })
-let confirmedMeetings = [meetingArr[0]]
-for (let i=1; i<meetingCnt; i++){
-    let lastMeeting = confirmedMeetings[confirmedMeetings.length-1]
-    if( lastMeeting[1] <= meetingArr[i][0]){
-        confirmedMeetings.push(meetingArr[i])
+    return a[1]-b[1];
+})
+let cnt = 1
+let lastMeeting = meetingArr[0]
+for(let i =1; i<meetingArr.length; i++){
+    let currentMeet = meetingArr[i]
+    if( lastMeeting[1] <= currentMeet[0]){
+        cnt += 1
+        lastMeeting = currentMeet
     }
 }
-console.log(confirmedMeetings.length)
+
+console.log(cnt)
