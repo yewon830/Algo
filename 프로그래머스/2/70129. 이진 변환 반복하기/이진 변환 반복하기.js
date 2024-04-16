bef67e7bae4d3c4d23b24f0751062058aacdde99
Ye Won Jeong <wonderwall00@naver.com>
2024-04-16 21:26:12 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 10.05 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -1,35 +1,9 @@
 function solution(s) {
-    let str = s; 
-    let stringLength = 0;
-    let [changeCnt, zeroCnt] =[0,0];
-    // 0 제거한 길이
-    while(true){
-        let answer = '';
-        for(let i=0; i<str.length;i++){
-            if(str[i]==='1'){
-                stringLength += 1
-            }else{
-                zeroCnt += 1
-            }
-        }
-        for(let i = 18; i>=0; i--){
-            if(stringLength >= (2**i)){
-                stringLength -= 2**i
-                answer += '1';
-            }else{
-                if(answer.includes('1'))
-                   answer += '0';
-            }
-        }
-        changeCnt += 1;
-        str = answer
-
-        if(str === '1'){
-            break
-        }
+    let answer = [0,0];
+    while(s !== '1'){
+        answer[1] += (s.match(/0/g)||[]).length;
+        answer[0]++
+        s = s.replace(/0/g,'').length.toString(2);
     }
-
-    return [changeCnt, zeroCnt];
-    }
-    
-    
+    return answer
+}
