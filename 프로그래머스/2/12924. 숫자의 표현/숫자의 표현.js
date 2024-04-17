be626884df66c8681cec55d3557e013c9af678aa
Ye Won Jeong <wonderwall00@naver.com>
2024-04-17 19:54:13 +0900

    [level 2] Title: 숫자의 표현, Time: 1.58 ms, Memory: 36.2 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -1,6 +1,10 @@
 function solution(n) {
-    var answer = 1;
-    for(let i=1;i<=n/2;i++){
+    var answer = 0;
+    //연속하는 자연수로 n을 표현하라(자기자신 포함)
+    // i에서 1에서부터 for문을 돌면서 n까지 간다(시작점)
+    //for문을 한개 더 돌면서 0에서 n-i까지 돌아서 하나씩 더한다.
+    //이 총합이 n이되면 answer+1하고 break
+    for(let i=1;i<=n;i++){
         let sum = 0;
         for(let j=0; j<=n-i;j++){
             sum += i+j;
