cb24b318fc95ab8c05c8224cd32e9961867a19ee
Ye Won Jeong <wonderwall00@naver.com>
2024-04-18 13:58:21 +0900

    [level 2] Title: 카펫, Time: 0.05 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -1,11 +1,18 @@
-function solution(brown, red) {
+
+function solution(brown, yellow) {
     var answer = [];
-    for (var i = 3; i <= (brown+red)/i; i++) {
-        var x = Math.floor((brown+red)/i);
-        if( (x-2)*(i-2)=== red) {
-            break;
+    //전체 가로세로-2를 해서 그 넓이가 yellow면은 답 
+    //i는 세로 길이, 가로 길이보다 더 커지는 순간 제외
+    for(let i=3; i<=(brown+yellow)/i;i++){
+        if((brown+yellow)%i===0){
+            let width = (brown+yellow)/i;
+            if((width-2)*(i-2)===yellow){
+                answer[0] = width;
+                answer[1] = i
+            }
         }
+
     }
 
-    return [x,i];
+    return answer;
 }
