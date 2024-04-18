c23de4939663840d7de2b9088c196f2bdecfe34e
Ye Won Jeong <wonderwall00@naver.com>
2024-04-18 19:37:53 +0900

    [level 2] Title: 피보나치 수, Time: 4.83 ms, Memory: 38.8 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -1,16 +1,14 @@
 function fibo(n){
     let dp = [];
-    dp[0] = 0n;
-    dp[1] = 1n;
-    for(let i = 2; i<=n; i++){
-        dp[i] = dp[i-1] + dp[i-2]
+    dp[0] = 0;
+    dp[1] = 1;
+    for(let i=2; i<=n; i++){
+        dp[i] = (dp[i-1] + dp[i-2]) % 1234567 
     }
     return dp[n]
 }
 
 function solution(n) {
-    var answer = '';
-    let fiboRes = fibo(n);
-    answer = fiboRes % BigInt(1234567);
-    return Number(answer.toString());
+    var answer = fibo(n);
+    return answer;
 }
