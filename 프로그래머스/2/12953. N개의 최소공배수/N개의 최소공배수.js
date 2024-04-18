f069204c747c8f80f6c280770264f6df1706768d
Ye Won Jeong <wonderwall00@naver.com>
2024-04-18 15:53:50 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
@@ -1,22 +1,10 @@
-function solution(arr) {
-    var answer = 0;
-    //몰라 일단 생각나는 거는 배열을 오름차순으로 정렬해서
-    //배열의 제일 큰 값의 배수가 다른 수의 배수가 되는지 확인.
-    arr.sort((a,b)=>a-b);
-    let i = 1;
-    while(true){
-        let flag = true;
-        for(let j=1;j<arr.length;j++){
-            if((arr[0]*i)%arr[j]!==0){
-                flag = false;
-                break
-            }
-        }
-        if(flag){
-            answer = arr[0] * i
-            break
-        }
-        i++
-    }
-    return answer;
+function gcd(a,b){
+    //a를 b로 나눈 나머지
+    //0이 되면 b
+    let r = a % b
+    return r===0? b:gcd(b,r)
+}
+function solution(arr){
+    let answer = arr.reduce((a,b)=> (a*b) / gcd(a,b))
+    return answer
 }
