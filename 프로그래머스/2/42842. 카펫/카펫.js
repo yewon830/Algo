e20bb4c22e3fdb6aaaae3d7f54e4f5c79cb2838e
Ye Won Jeong <wonderwall00@naver.com>
2024-04-18 13:19:21 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -1,17 +1,11 @@
-function solution(brown, yellow) {
+function solution(brown, red) {
     var answer = [];
-    //갈색이 노란색을 둘러싸고 있음.
-    //에라토스테네스 체
-    for(let i=1; i<=Math.sqrt(yellow);i++){
-        if(yellow % i === 0){
-            let width = Math.max(i, yellow/i);
-            let height = yellow/width;
-            if(brown + yellow === (width+2)*(height+2)){
-                answer[0] = width+2;
-                answer[1] = height+2;
-            }
+    for (var i = 3; i <= (brown+red)/i; i++) {
+        var x = Math.floor((brown+red)/i);
+        if( (x-2)*(i-2)=== red) {
+            break;
         }
     }
-    return answer;
-    
+
+    return [x,i];
 }
