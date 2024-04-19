eea24c58f344f7ae6d6b8f6e8558eb5a32eb0ae1
Ye Won Jeong <wonderwall00@naver.com>
2024-04-19 17:56:27 +0900

    [level 2] Title: 구명보트, Time: 28.07 ms, Memory: 38 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -0,0 +1,17 @@
+function solution(people, limit) {
+    var answer = 0;
+    let left = 0;
+    let right = people.length-1;
+    people.sort((a,b)=>a-b);
+    while(left<=right){
+        if(people[left] + people[right] > limit){
+            right--;
+            answer++;
+        }else{
+            left++;
+            right--;
+            answer++;
+        }
+    }
+    return answer;
+}
