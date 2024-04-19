c02502cee8edf02684f03c9c4e81cce6e92a1227
Ye Won Jeong <wonderwall00@naver.com>
2024-04-19 18:18:24 +0900

    [level 2] Title: 구명보트, Time: 10.29 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.js
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -1,17 +1,13 @@
 function solution(people, limit) {
     var answer = 0;
-    let left = 0;
-    let right = people.length-1;
+    let i = 0;
     people.sort((a,b)=>a-b);
-    while(left<=right){
-        if(people[left] + people[right] > limit){
-            right--;
-            answer++;
-        }else{
-            left++;
-            right--;
-            answer++;
+    //i는 사람을 같이 태우는 경우.
+    // 모두 각자 다른 보트에 타는 경우 - 같이 타는 경우(i)
+    for(let j = people.length-1; i<j; j--){
+        if(people[i]+ people[j] <= limit){
+            i++
         }
     }
-    return answer;
+    return people.length - i;
 }
