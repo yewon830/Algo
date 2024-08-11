3af9a229a7bf74826873ff362091013cc3ba623f
Ye Won Jeong <wonderwall00@naver.com>
2024-08-11 15:33:59 +0900

    [level 2] Title: 문자열 압축, Time: 4.84 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
@@ -1,29 +1,26 @@
 def solution(s):
-    result=[]
-    
-    if len(s)==1:
+    answer = []
+
+    if len(s) == 1:
         return 1
-    
-    for i in range(1, len(s)+1):
-        b = ''
+
+    for i in range(1, len(s)//2 + 1):
+        result = ''
+        tmp = s[:i]
         cnt = 1
-        tmp=s[:i]
-
-        for j in range(i, len(s)+i, i):
-            
-            if tmp==s[j:i+j]:
-                cnt+=1
+        for j in range(i, len(s), i):
+            if tmp == s[j:j+i]:
+                cnt += 1
             else:
-                if cnt!=1:
-                    b = b + str(cnt)+tmp
+                if cnt > 1:
+                    result += str(cnt) + tmp
                 else:
-                    b = b + tmp
-                    
-                tmp=s[j:j+i]
+                    result += tmp
                 cnt = 1
-                
-        result.append(len(b))
-        
-
-    return min(result)
-
+                tmp = s[j:j+i]
+        if cnt > 1:
+            result += str(cnt) + tmp
+        else:
+            result += tmp
+        answer.append(len(result))
+    return min(answer)
