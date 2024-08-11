08ea5a43ec6bdacadc02fd1bb841139358d909c7
Ye Won Jeong <wonderwall00@naver.com>
2024-08-11 15:38:40 +0900

    [level 2] Title: 문자열 압축, Time: 3.77 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/README.md
+++ 프로그래머스/2/60057. 문자열 압축/README.md
@@ -1,10 +1,10 @@
 # [level 2] 문자열 압축 - 60057 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60057) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60057#) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 4.84 ms
+메모리: 10.2 MB, 시간: 3.77 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 11일 15:33:56
+2024년 08월 11일 15:38:38
 
 ### 문제 설명
 
--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
@@ -1,26 +1,36 @@
+import sys
 def solution(s):
-    answer = []
-
-    if len(s) == 1:
+    # 압축 단위를 늘려가면서 슬라이싱
+    # 슬라이싱 했을 때 연속하면 cnt 증가
+    # 연속하지 않는다면 cnt = 1로 갱신, 압축 문자열 추가(1은 따로)
+    if len(s)==1:
         return 1
-
-    for i in range(1, len(s)//2 + 1):
-        result = ''
-        tmp = s[:i]
+    max_cnt = sys.maxsize
+    for i in range(1, len(s)//2 +1): #압축 단위
+        # 첫 시작점
         cnt = 1
-        for j in range(i, len(s), i):
-            if tmp == s[j:j+i]:
+        tmp = s[:i] 
+        compressed = ''
+        for j in range(i,len(s),i):
+            new_s = s[j:j+i]
+            if new_s == tmp:
                 cnt += 1
-            else:
-                if cnt > 1:
-                    result += str(cnt) + tmp
+            else: #다른 문자열
+                if cnt >1:
+                    compressed += str(cnt) + tmp
+                    cnt = 1
                 else:
-                    result += tmp
-                cnt = 1
-                tmp = s[j:j+i]
-        if cnt > 1:
-            result += str(cnt) + tmp
+                    compressed += tmp
+                tmp = new_s
+        #마지막 다 돌았을 때 문자열을 추가
+        if cnt >1 :
+            compressed += str(cnt) + tmp
         else:
-            result += tmp
-        answer.append(len(result))
-    return min(answer)
+            compressed += tmp
+        
+
+        # 이 시점에서 문자열 완성
+        if len(compressed) < max_cnt:
+            max_cnt = len(compressed)
+        
+    return max_cnt
