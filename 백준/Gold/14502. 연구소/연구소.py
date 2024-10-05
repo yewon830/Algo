9f462829d202d30c0d5002f4aaa14b1fb4164a79
Ye Won Jeong <wonderwall00@naver.com>
2024-10-05 23:56:51 +0900

    [Gold IV] Title: 연구소, Time: 432 ms, Memory: 115092 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,58 +1,60 @@
-
 import sys
-from collections import deque
 from itertools import combinations
-
-# 연구소는 크기가 N*M
-# 벽은 3개
-# 바이러스 2 벽 1 빈칸 0
-
-# 벽을 3개 세우는 조합을 찾고, 바이러스를 퍼트리기.;
+from collections import deque
 input = sys.stdin.readline
 n, m = map(int,input().split())
 board = [list(map(int,input().split())) for _ in range(n)]
 
+# 벽 세우기
+# 0은 빈칸, 1은 벽, 2는 바이러스. 
+# 벽을 3개 세운 뒤, 
+# 바이러스를 퍼트리고(BFS)
+# 0인 영역의 최댓갑 구하기
 
-empty, virus = [],[]
+# 벽 3개 세우기
+# 0인 곳의 좌표를 만들어서, 임의로 3개의 벽을 만든다(완전탐색, 조합)
+empty = []
+virus = []
+answer = 0
 for i in range(n):
     for j in range(m):
         if board[i][j] == 0:
             empty.append((i,j))
         elif board[i][j] == 2:
             virus.append((i,j))
-
-max_cnt = 0            
-def bfs():
-    global max_cnt
+            
+def bfs(board):
     queue = deque(virus)
-    tmp = [board[i][:] for i in range(n)]
+    temp = [board[i][:] for i in range(n)]
+    global answer
     
     while queue:
-        cr, cc= queue.popleft()
-        for dr, dc in [[0,-1],[0,1],[1,0],[-1,0]]:
+        cr,cc = queue.popleft()
+        for dr, dc in [[0,1],[0,-1],[1,0],[-1,0]]:
             nr = dr + cr
             nc = dc + cc
-            # 범위 내의, 0인거
-            if 0<= nr< n and 0<= nc <m and tmp[nr][nc] == 0:
-                tmp[nr][nc] = 2
+            if 0<=nr<n and 0<= nc<m and temp[nr][nc] == 0:
+                temp[nr][nc] = 2
                 queue.append((nr,nc))
+    #0 개수 세기
     cnt = 0
     for i in range(n):
         for j in range(m):
-            if tmp[i][j] == 0:
+            if temp[i][j] == 0:
                 cnt += 1
-    if max_cnt < cnt:
-        max_cnt = cnt
+    if answer < cnt:
+        
+        answer = cnt
+    return
 
-for walls in combinations(empty,3):
-    row = []
-    col = []
-    for r,c in walls:
-        row.append(r)
-        col.append(c)
+# 3개의 벽 만들기
+
+for comb in combinations(empty,3):
     for i in range(3):
-        board[row[i]][col[i]] = 1 #벽세우기
-    bfs()
+        board[comb[i][0]][comb[i][1]] = 1 # 벽세우기
+    # print(board)
+    bfs(board)#바이러스 퍼트리기
     for i in range(3):
-        board[row[i]][col[i]] = 0
-print(max_cnt)
+        board[comb[i][0]][comb[i][1]] = 0 #원상태로 돌리기
+
+print(answer)
